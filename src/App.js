d2d33fe3854f9f7cf623c7a95fd353c6d712e770
liammcnabola <liam.mcnabola@protonmail.com>
2024-02-27 21:55:50 +0000

    item component - not finished

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import Home from './Home/Home';
+import Item from './Components/Item/Item';
 
 function App() {
   return (
@@ -13,12 +14,14 @@ function App() {
         <header>
           <nav>
             <Link to='/'></Link>
+            <Link to='/items'></Link>
 
           </nav>
         </header>
 <Routes>
  
  <Route path='/' element={<Home/>}></Route>
+ <Route path='/item' element={<Item/>}></Route>
 
 
 </Routes>
---
+++ src/Components/Item/Item.jsx
@@ -0,0 +1,84 @@
+import axios from "axios";
+import { useState } from "react";
+
+function Item() {
+
+    const [id, setId] = useState("");
+    const [itemName, setItemName] = useState("");
+    const [price, setPrice] = useState("");
+    const [quantity, setQuanity] = useState("");
+    const [items, setItems] = useState([]);
+
+    function getItem() {
+
+        axios.get("http://localhost:8082/items/get").then((response) => {
+            setItems(response.data)
+
+        })
+
+        function checkItem() {
+
+            axios.get("http://localhost:8082/item/get").then(response => {
+                console.log(response)
+                for (const items of response.data) {
+                    if (items.itemName === itemName && items.price === price) {
+                        alert("Item already listed")
+                        return;
+                    }
+                }
+            })
+        }
+        axios.post("http://localhost:8082/item/create",
+            {
+                itemName,
+                price,
+                quantity
+            })
+            .then(response => {
+                console.log(response);
+                setItemName("");
+                setPrice("");
+                setQuanity("");
+                setItems();
+            }).catch(err => console.error(err))
+    }
+return
+
+}
+
+ 
+    <div>
+
+        <form onSubmit={(e) => {
+            e.preventDefault();
+            checkItem();
+
+        }} />
+
+        <h1>Items</h1>
+        <label>Item Name</label>
+        <input>
+            value={itemName}
+            onChange={(e) => setItemName(e.target.value)}
+        </input>
+        <label>Item Price</label>
+        <input>
+            value={price}
+            onChange={(e) => setPrice(e.target.value)}
+        </input>
+        <label>Item Quanity</label>
+        <input>
+            value={quantity}
+            onChange={(e) => setQuantity(e.target.value)}
+        </input>
+
+
+
+
+
+
+
+    </div>
+
+
+export default Item
