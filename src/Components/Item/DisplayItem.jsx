83a94f01fd30762a679aaa9658b355ba1aee94b4
liammcnabola <liam.mcnabola@protonmail.com>
2024-02-28 15:26:16 +0000

    display items?

--- src/Components/Item/DisplayItem.jsx
+++ src/Components/Item/DisplayItem.jsx
@@ -1,33 +1,36 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-function DisplayItem(){
-    const itemList = []
-    const [item, setItem] = useState([]);
+function DisplayItem() {
+  const [items, setItems] = useState([]);
 
-    function getItem(){
-        axios.get("http://localhost:8082/item/get")
-        .then((response) => {setItem(response.data)})
-        .catch(console.log())}
-        useEffect(()=> {getItem()},[])
-            for (const item of items){
-                itemList.push(
-                    item.id
-                )
-            }
-            
+  function getItem() {
+    axios
+      .get("http://localhost:8082/item/get")
+      .then((response) => {
+        setItems(response.data);
+      })
+      .catch(console.log);
+  }
 
-            return(
-                {/* {itemList} */ }
+  useEffect(() => {
+    getItem();
+  }, []);
 
-            );
+  const itemList = [];
 
+  for (const singleItem of items) {
+    itemList.push(
+      <div key={singleItem.id}>
+        {singleItem.id} - {singleItem.itemName} - {singleItem.price}
+      </div>
+    );
+  }
 
-    }
-
-
-
-
-
+  return <div>{itemList}</div>;
+}
 
 export default DisplayItem;
+
+
+
