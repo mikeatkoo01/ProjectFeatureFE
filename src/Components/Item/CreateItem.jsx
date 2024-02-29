5adbadcee20c9910b6d1cd3b685b567c71d0bc6c
liammcnabola <liam.mcnabola@protonmail.com>
2024-02-29 12:15:39 +0000

    fixed issue preventing adding a new item not already in SQL, but duplicate item catch still not working

--- src/Components/Item/CreateItem.jsx
+++ src/Components/Item/CreateItem.jsx
@@ -15,7 +15,7 @@ function CreateItem() {
         if (
           item.name &&
           item.price &&
-          item.name.toLowerCase() === name.toLowerCase() &&
+          item.name.toLowerCase() !== name.toLowerCase() &&
           item.price.toLowerCase() === price.toLowerCase()
         ) {
           alert("Item already listed");
